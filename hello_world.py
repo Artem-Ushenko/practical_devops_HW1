e898e6119a5449ec7eae7aa44aa131ce16b50230
Artem Ushenko <kingart030192kingart@gmail.com>
2023-08-28 17:00:00 +0300

    Updated hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,7 @@
+"""
+This is a simple "Hello, World!" module.
+
+This module demonstrates the basic structure of a Python module
+and prints the classic "Hello, World!" message.
+"""
 print("Hello, World!")
